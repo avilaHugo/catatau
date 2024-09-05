47cbb59e244cc9cc59af296b3f5a2f3498d01612
Hugo Ávila <53014804+avilaHugo@users.noreply.github.com>
2024-09-04 22:43:21 -0300

    Let's do the basics first.

--- src/catatau.py
+++ src/catatau.py
@@ -4,41 +4,45 @@
 from __future__ import annotations
 
 import argparse
-import functools as ft
 import collections.abc as c
-from typing import Self
-from numbers import Number
-import itertools as it
+import typing as t
+from functools import reduce
 
-class 
 
-class Stream(c.Iterator):
-    def __init__(
-        self,
-        handle: str,
-        separator: str = ";",
-        header: c.Iterable[str | int] | None = None
-    ) -> None:
-        self.data = self._init_stream(handle)
-        self.current = next(self.data)
-        self.header = header if header else tuple(range(0, len(self.current)))
+class Series(c.Mapping):
+    def __init__(self, data: c.Sequence, name: None) -> None:
+        self.data = data
+        self.name = name
 
-    def _init_stream(self) -> c.Iterator[Row]:
-        with open(self.handle, mode='r') as f:
-            yield from map(lambda line: line.rstrip('\n').split(self.separator), f)
 
-    def __iter__(self) -> Self:
-        return self
-    
-    def __next__(self) -> c.Generator[Row, None, None]:
-        _next = next(self.data, None)
-        if None:
-            raise StopIteration
-        yield next
+class DataFrame(c.Mapping):
+    def __init__(self, data: dict[str | int, Series]) -> None:
+        self.data = data
+
+
+def iter_file(file_name: str) -> c.Iterator[str]:
+    with open(file_name, mode="r") as f:
+        yield from map(lambda line: line.rstrip("\n"), f)
+
+
+def compose(*functions: c.Callable) -> c.Callable:
+    return reduce(
+        lambda inner, outter: lambda value: outter(inner(value)),
+        functions,
+        lambda value: value,
+    )
+
+
+# def read_csv(file_name: str, separator=",") -> DataFrame:
+#     return DataFrame(dict(zip())
+
 
 def main(file_names: c.Iterable[str]) -> None:
     for file_name in file_names:
-        print(Stream(handle=file_name))
+        print(*map(lambda line: line.split(";"), iter_file(file_name)), sep="\n")
+        compose(
+            iter_file,
+        )(file_name)
 
 
 if __name__ == "__main__":
